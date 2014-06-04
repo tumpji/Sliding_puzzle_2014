dfaa1389fde8145eb4615fdef6ce8e3d38ce6164
Jiri Tumpach <tumpji@gmail.com>
2014-06-04 02:26:35 +0200

    Pridana funkcionalita k testum modulu object

---
+++ tests/test_object/test_object.cc
@@ -0,0 +1,68 @@
+#include <string>
+#include <cctype>
+#include <iostream>
+#include "../../object.h"
+
+using namespace std;
+
+void test_num1 ( char [CONST::policek] );
+
+void parser ( void )
+{
+	string radek;
+	char predane [ CONST::policek ];
+	
+	while ( true )
+	{
+		getline ( std::cin , radek  );
+		
+		if ( radek == "getbest" )
+			continue;
+		else if ( radek == "end" )
+			break;
+		else if ( radek.size() == 0 )
+			continue;
+	
+		int pozice_predane = 0;	
+		int pos = 0;
+		predane[0] = 0;
+
+		for ( ; pos < radek.length() ; ++pos ) // iteruje prez radek
+		{
+			if ( isdigit(radek[pos] ) )
+			{
+				predane[pozice_predane] *= 10;
+				predane[pozice_predane] += radek[pos] - '0';
+			}
+			else if ( radek[pos] == ',' )
+			{
+				pozice_predane += 1;
+				if ( pozice_predane != CONST::policek )
+					predane[ pozice_predane ] = 0;
+			}
+		}
+
+		if ( pozice_predane != 16 ) {
+			cerr << "Nespravny format " << endl;
+			cerr << "Predano: " << radek << endl;
+			cout << endl;
+			continue;
+		}
+		
+		test_num1 ( predane );
+	}
+
+}
+
+void test_num1 ( char hodnoty[CONST::policek] )
+{
+	OBJECT objekt ( hodnoty );
+	OBJECT * vysledek = objekt.generate_best_children ();
+	vysledek->print_obj_parser ();
+}
+
+int main ( void ) 
+{
+	parser ( );
+	return 0;
+}
--- tests/test_object/test_object.py
+++ tests/test_object/test_object.py
@@ -9,17 +9,21 @@ import time
 # hleda chyby v modulu object.o
 class Rozhrani:
 	def __init__ ( self ):
-		self.program = IOmodule.IOmodule ( "./prog.run" )
+		self.program = IOmodule.IOmodule ( "./test_object.run" )
 
 	def __del__ ( self ):
 		pass
 
-	def predej_programu ( self , policka ):
-		temp_string = "("	
+	def process_var_set ( self , policka ):
+		temp_string = "set: ("	
 		for number in policka:
 			temp_string += str(number) + ","
 		temp_string += ")\n"
 		self.program.Write ( temp_string )
+
+	def process_send_command ( self , command ):
+		command += "\n"
+		self.program.Write ( command )
 
 	def check_output ( self , tuple_tuple ):
 		if len(tuple_tuple) > 4: # nemuze se vratit vice
@@ -79,37 +83,75 @@ class TestModule:
 	def __init__ ( self ):
 		self.policka = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 		self.output = None
-		print( "INICIALIZACE" )
 		self.rozhrani = Rozhrani()
+		self.number_of_test = 0
+		self.current_test = 0
 
 	def __del__ ( self ):
-		print ( "DESTRUKTOR" )
+		pass
 
+	# nastavi "nahodnnou" hodnou v self.policka a nastavi jaky test se pojede
 	def get_random ( self ):
 		random.shuffle( self.policka ) #prohazi to
+		self.current_test =  random.randint ( 0 , 1 )
 
+	# tiskne vstup a vystup z testu v pripade chyby
+	def print_error_msg ( self ): # tiskne puvodni a predany
+		string = "Puvodni:__________\n"
+		for i in range ( 0 , 16 ):
+			if i % 4 == 0:
+				string += "|"
+			string += "{0:^4}".format(str(self.policka[i]))
+			if i % 4 == 3:
+				string += "|\n"
+		string += "^" * (4*4 + 2 ) 
+		print ( string )
+		print ( "vysledne:" )
+		zasobnik = [ str(), str(), str(), str(), str(), str(), ]
+		for out in self.output:
+			zasobnik[0] += "_" * ( 4*4 + 2 )
+			zasobnik[5] += "^" * ( 4*4 + 2 )
+			if len(zasobnik[0] ) > 19:
+				zasobnik[0] += "_"
+				zasobnik[5] += "^"
+			for index in range ( 0 , len(out) ):
+				if index % 4 == 0:
+					zasobnik[index//4 + 1] += "|"
+				zasobnik[index//4 + 1] += "{0:^4}".format(str(out[index]))
+				if index % 4 == 3:
+					zasobnik[index//4 + 1] += "| "
+		for string in zasobnik:	
+			print ( string )
+		print ( "#"*40 )		
+
+	# zjisti jake dva idnexy se oproti poslanemu zmenily
+	# vraci MyError jestli se nezmenili 2
 	def get_dismatch_indexes ( self , predany_tuple ):
 		ret_val = tuple()
 		for index in range( 0 , 15 ):
 			if self.policka[index] != predany_tuple[index]:
 				ret_val += index,
 		if len(ret_val) != 2:
-			self.tisk_chyby ()
+			raise IOmodule.MyError( "Nesplnuje pozadavky" )
 		return ret_va
 
+	# kontroluje jestli test dopadl uspesne nebo ne
 	def check_output ( self ):
-		print ( "moje :" + str( self.policka ) )
-		print ( "tvoje:" + str( self.output  ) ) 
-		for i in range( 0 , 15 ):
-			if ( self.policka[i] != self.output[0][i] ):
-				print ( "Chyba" )
-
+		try:
+			dismatch_tuple = self.get_dismatch_indexes ( self.output[0])
+			print ( "Test probehl uspesne" )
+		except IOmodule.MyError: 
+			self.print_error_msg ()
+	
+	# generuje ruzne testy	
 	def Post_test ( self ):
-		print ( "TEST" )
-		self.get_random () # prohazeni
-		self.rozhrani.predej_programu ( self.policka )
-		self.output = self.rozhrani.preber_od_programu ()
-		self.check_output ()
+		for i in range( 0 , 10 ):
+			self.get_random () # prohazeni
+			self.rozhrani.process_var_set ( self.policka )
+			self.rozhrani.process_send_command ( "getbest" )
+			self.output = self.rozhrani.preber_od_programu ()
+			self.check_output ()
+			
 
 	
 		
@@ -118,29 +160,6 @@ program.Post_test ()
 		
 		
 
-exit (0 )
-# inicializace
-x = Rozhrani ()
-
-# vola generate_random
-x.predej_programu ()
-x.generate_random ()
-x.predej_programu ()
-
-listy = x.preber_od_programu ()
-
-a = 0
-for li in listy:
-	print ( li )
-	a += 1
-
-listy = x.preber_od_programu ()
-
-for li in listy:
-	print ( li )
-	a += 1
-
-print ( "pocet :" + str( a ) )
 
 
 
