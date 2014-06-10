8ecdaa223ba2244574e99b1ed4ddb6a406b41223
Jiri Tumpach <tumpji@gmail.com>
2014-06-11 00:42:00 +0200

    Refaktorizace solving_engine.cc

--- solving_engine.cc
+++ solving_engine.cc
@@ -23,7 +23,6 @@ static bool zkontroluj_vstup ( const char predane [ CONST::policek ] )
 Engine::Engine ()
 {
 	array_obj = new OBJECT[500];	
-
 }
 
 Engine::~Engine ()
@@ -31,14 +30,14 @@ Engine::~Engine ()
 	delete [] array_obj;
 }
 
+// implementace IDA*
 int Engine::set_up_and_run ( const char predane [CONST::policek] )
 {
-	int max_depth = 498;
-	int depth_heur = 1;
-	int position = 0;
-	unsigned int __children_generated = 0;
 	std::set<OBJECT> zasobnik_pouzivanych;
-	
+	unsigned int children_generated = 0; 
+	const int max_depth = 498;
+	int depth_heur;
+	int position = 0;
 	
 	if ( !zkontroluj_vstup ( predane ) )
 		return 0;
@@ -51,11 +50,9 @@ int Engine::set_up_and_run ( const char predane [CONST::policek] )
 	{
 		new ( array_obj ) OBJECT ( predane ); // inicializace korene 
 		position = 0;
-		std::cout << depth_heur  << "\t" << __children_generated << std::endl;
+		std::cout << depth_heur  << "\t" << children_generated << std::endl;
 		assert ( zasobnik_pouzivanych.size ()  <= 1 );
 		zasobnik_pouzivanych.insert ( *array_obj );
-
-//	best_heur_obj.print_obj();
 
 		while ( position >= 0 )
 		{
@@ -63,58 +60,34 @@ int Engine::set_up_and_run ( const char predane [CONST::policek] )
 			while ( position + array_obj[position].get_heuristic() < depth_heur ) 
 			{
 				array_obj[position].generate_best_children_next_to ();
-				// predpoklada se ze zde se nemuze nic stat 
+				// nemuze nastat chyba
 				++position;
 				if ( !zasobnik_pouzivanych.insert( array_obj[position] ).second )
-					break;
+					break; // jestlize jsem toto uz videl
 
-//				for ( int pos = 0 ; pos < position - 1 ; ++pos )
-//					if ( array_obj[pos] == array_obj[position] )
-//						break;
-#if 0
-	if ( best_heur_obj.get_heuristic () >= array_obj[position].get_heuristic() )
-		best_heur_obj = array_obj[position];
-				++__children_generated;
-#endif
-				if ( array_obj[position].is_sorted () ) // je to cilovy obrazec ?
+				++children_generated;
+				// je to cilovy obrazec ?
+				if ( array_obj[position].get_heuristic() == 0 ) 
 					return predej_vysledek ( array_obj, position );
-//				if ( __children_generated  )
-//					best_heur_obj.print_obj ();
 			}
 
-#if 0
-		static int uz_byl = 0;
-			if ( uz_byl )
-				{ 
-					array_obj[position].print_obj (); 
-					std::cout << array_obj[position].get_heuristic () << std::endl;
-					--uz_byl; 
-				}
-#endif
-			while ( --position >= 0 ) // backtracking jen do korene
+			while ( --position >= 0 ) // backtrace jen do korene
 			{
+				// vyjmout z pouzivanych dat
 				zasobnik_pouzivanych.erase ( array_obj[position+1] );
+				// pokud neni mozne vytvorit dalsi dite
 				if ( array_obj[position].generate_best_children_next_to () )
 					continue;
+				  // je tento vygenerovany obj. mensi nez omezeni
 				else if ( position + array_obj[1+position].get_heuristic() < depth_heur )
 				{	
-					++position;
-
+					++position; // ano -> do zasobniku
 					if ( !zasobnik_pouzivanych.insert( array_obj[position] ).second )
-						continue;
+						continue; // pokud uz je v zasobniku
 				
-//					for ( int pos = 0 ; pos < position - 1 ; ++pos ) // kdyz se najde v seznamu
-//						if ( array_obj[pos] == array_obj[position] )
-//							continue;	
-				++__children_generated;
-#if 0
-					
-	if ( best_heur_obj.get_heuristic () >= array_obj[position].get_heuristic() )
-		best_heur_obj = array_obj[position];
-//				if ( __children_generated  )
-//					best_heur_obj.print_obj ();
-#endif
-					if ( array_obj[position].is_sorted () ) // je to cilovy obrazec ?
+					++children_generated;
+					// je to cilovy obrazec ?
+					if ( array_obj[position].get_heuristic () == 0 ) 
 						return predej_vysledek ( array_obj, position );
 					break;
 				}
@@ -123,7 +96,8 @@ int Engine::set_up_and_run ( const char predane [CONST::policek] )
 			
 		} // 1. while 
 	} // for end 
-	return 0;
+
+	return 0; // nepravdepodobne
 }
 
 int Engine::predej_vysledek ( OBJECT* start, int& position )
