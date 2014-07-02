85a7418a81230d7f9a0a8bc8365ad44c020c10af
Jiri Tumpach <tumpji@gmail.com>
2014-07-02 21:58:21 +0200

    Zpresneni vypoctu

--- main.cc
+++ main.cc
@@ -37,16 +37,11 @@ double time_difference = (	( time_now.tv_sec - time_beg.tv_sec ) +
 
 static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsucessful );
 
-static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsucessful)
+// to co se deje po neuspesnem pokusu
+static void thinking_simulation_unsucessful ( const float & last_wait , unsigned & unsucessful )
 {
-	static bool message_imposible = false;
-	static float last_wait = 0;
-	if ( pos == 0 ) message_imposible = false;
-
 	switch ( unsucessful )
 	{
-		case 0: // neni proc
-			break;
 		case 1:
 		case 2: // nejrychleji
 			my_sleep( last_wait / 2. );
@@ -56,10 +51,72 @@ static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsuce
 			my_sleep ( last_wait / 2. * (float)unsucessful );
 			break;
 	}
+}
 
-	const static float minimum_time = 1.f/( 3.6f ); 	// maximalni frekvence ( click/sec )
-	// minimalni rozdil mezi optimem a maximem 
-	const static float minimum_opt = 1.f/( 3.2f ); // nejvetsi rychlost prumerna
+static void thinking_simulation_last ( 	double & to_end , 
+					float& optimum , 
+					float& last_wait,
+					unsigned vzdalenost_od_posledniho )
+{
+	
+	std::cout << "th_sim  ";
+	switch ( vzdalenost_od_posledniho )
+	{
+		case 0 :  // posledni
+			if ( to_end >= ( 1/3.5 ) ) // je to vetsi nez nejrychlejsi co se da stihnout ?
+			{
+				last_wait = to_end; // presne
+				my_sleep( last_wait );
+			}
+			else // nelze to stihnout 
+			{
+				last_wait = 1/3.5; // nejrychlejsi vyprodukovany
+				my_sleep( last_wait );
+			}
+			break;
+
+		case 1 : // predposledni
+			if ( to_end/2. >= ( 1/3.5 ) )
+			{
+				// minimum + 2/3 toho co je navic
+				last_wait = 1/3.5 + (to_end/2. - (1/3.5))*2./3. ;
+				my_sleep( last_wait );
+			}
+			else  
+			{	// jsme pozadu musi se dohnat to co jde ale nenapadne
+				last_wait = 1/3.5;
+				my_sleep( last_wait );
+			}
+			break;
+		case 2 : // predpredposledni
+			if ( to_end/3. >= ( 1/3.5 ) )
+			{
+				last_wait = 1/3.5 + (to_end/3. - (1/3.5))*3./5.;
+				my_sleep( last_wait );
+			}
+			else
+			{
+				last_wait = 1/3.4;
+				my_sleep ( last_wait );
+			}
+			break;
+	}
+	return;
+}
+
+static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsucessful)
+{
+#ifndef NDEBUG
+	std::cout << "thinking_simulation : " << size << "  " << pos << "   " << unsucessful << std::endl;
+#endif
+
+	static bool message_imposible = false;
+	static float last_wait = 0;
+	if ( pos == 0 ) message_imposible = false;
+	if ( unsucessful ) { thinking_simulation_unsucessful ( last_wait , unsucessful ); return ; }
+
+	const static float minimum_time = 1.f/( 3.5f );	// maximalni frekvence ( click/sec )
+	const static float minimum_opt = 1.f/( 3.25f );	// nejvetsi rychlost prumerna
 
 	timespec now; // aktualni cas
 	clock_gettime ( CLOCK_REALTIME, &now ); 
@@ -69,43 +126,19 @@ static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsuce
 
 	float optimum = to_end/(float)( size-(pos+1) ); // sec/click
 	float diff = optimum - minimum_time;
-	int rand_modulo = (diff*2.*1e7);
+	int rand_modulo = (diff*2.*1e7)*5./6.;
 
 	PRINT( to_end );
 	PRINT( diff );
 	PRINT( optimum );
 
-	if ( size == pos + 1 && !message_imposible ) // posledni kousek --- vylepseni presne konvergence
-	{ // snazime se trefit presne do casu
-		if ( to_end >= minimum_time ) {
-			last_wait = to_end;
-			my_sleep( last_wait );  // presne 
-		}
-		else {
-			last_wait = minimum_time;
-			my_sleep( minimum_time ); // nepresne 
-		}
-		return; 
-	}
-	else if ( size == pos + 2 && !message_imposible ) // predposledni
-	{
-		if ( optimum > minimum_time ) {
-			rand_modulo = diff*1e7; // polovicni roztec
-			PRINT( rand_modulo );
-			rand_modulo = rand()%rand_modulo;
-			last_wait = rand_modulo + diff/2. + minimum_time; 
-			my_sleep ( last_wait );
-			return ;
-		}
-		else {
-			last_wait = minimum_time ;
-			my_sleep( last_wait );
-		}
+	if ( size - ( pos + 1) < 3 && !message_imposible ) // predposledni a posledni cekani
+	{	thinking_simulation_last ( to_end , optimum, last_wait, size - (pos+1) );
 		return;
 	}
 
 	if ( optimum < minimum_opt || message_imposible ) {
-		rand_modulo = 2.*((1./1.8) - minimum_time)*1e7; // velke opt
+		rand_modulo = 2.*((1./3.2) - minimum_time)*1e7; // velke opt
 		if ( !message_imposible ) {
 			if ( size - ( pos + 1 ) > 4 ) // neplati kdyz se uz blizime 
 				message_imposible = true;
@@ -116,11 +149,8 @@ static void thinking_simulation ( unsigned size , unsigned pos , unsigned unsuce
 	PRINT( rand_modulo );
 
 	assert( rand_modulo  > 0 );
-	rand_modulo = rand()%rand_modulo;
-
-	float time_wait = ((double)rand_modulo)/1e7 + minimum_time ;
-
-	std::cout << "sleep: " << time_wait << std::endl;
+	rand_modulo = (rand()%rand_modulo);
+	float time_wait = rand_modulo/1e7 + minimum_time;
 	last_wait = time_wait;
 	my_sleep( last_wait );
 }
@@ -152,6 +182,7 @@ int main ( int argc , char* argv[] )
 {
 	const char * predane;
 	double pozadovany_cas;
+	float nej_cas = INFINITY;
 	std::cout << "Pozadovany cas = " << std::flush;
 	std::cin >> pozadovany_cas;
 	std::cin.ignore( 100 , '\n' );
@@ -165,19 +196,33 @@ int main ( int argc , char* argv[] )
    while( 1 ){ 
 	std::cout << "Click -> poskladej puzzle " << std::endl;
 	predane = klik_poskladej_puzzle ( argc , rozhrani );
-	sleep ( 20 );
+
+	for ( int x = 0 ; x < 4*4 ; ++x )
+		std::cout << (int)predane[x] << "  " ;
+	std::cout << std::endl;
+	
+	my_sleep ( 20 ); // delete
+
 	init_timer ( pozadovany_cas );
 	// zacina se odpocitavat
 	std::cout << "Zacinam pocitat ... " << std::endl;
 	postup = solving_engine.run ((unsigned char *)predane);
+
+#ifndef NDEBUG
+	for ( std::pair<unsigned,OBJECT> p : postup )
+		p.second.print();
+#endif
+
 	std::cout << "Vypocet skoncil, zacina skladani ..." << std::endl;
 	skladani_policek ( rozhrani, postup, predane ); // posklada policka
 	
+// zaverecne zhodnoceni
 	clock_gettime( CLOCK_REALTIME , &time_str.time_end ); // zacne se odpocitavat
-	std::cout << "Hotovo za : " << ( time_str.time_end.tv_sec - time_str.time_beg.tv_sec) +
-				( time_str.time_end.tv_nsec - time_str.time_beg.tv_nsec )/1e9
-		<< std::endl;
-
+	double cas = ( time_str.time_end.tv_sec - time_str.time_beg.tv_sec) +
+		( time_str.time_end.tv_nsec - time_str.time_beg.tv_nsec )/1e9;
+	std::cout << "Hotovo za : " << cas << std::endl;
+	if ( cas < nej_cas ) nej_cas = cas;
+	std::cout << "Nejlepsi je : " << nej_cas << std::endl;
 	std::cout << "Cekam 3s" << std::endl;
 	sleep ( 3 );
   }
@@ -193,6 +238,7 @@ int main ( int argc , char* argv[] )
 static const char * klik_poskladej_puzzle ( int argc, Comunication& rozhrani )
 {
 	const char * predane = nullptr;	
+	bool complete = false;
 
 	if ( argc < 2 )  { // jestli je to bez arg.
 		std::cout << "Cekam na start ... " << std::endl;
@@ -201,16 +247,17 @@ static const char * klik_poskladej_puzzle ( int argc, Comunication& rozhrani )
 		std::cin.get(); 
 	}
 
-	std::cout << "Cekam 2s ... " << std::endl;
-	sleep ( 2 ); // pockame 
-	
 	rozhrani.poskladej_puzzle_click ();
 
 	do { // cekej dokud se nenachysta hraci pole
-		usleep( 1000 );
+		usleep( 10000 );
 		predane = rozhrani.preber_usporadani();
+		if ( predane == nullptr ) continue;
+		
+		for ( int x = 0 ; x < 4*4 - 1 ; ++x )
+			if ( predane[x] != x + 1 ) complete = true; // neni to or. obrazovka
 	}
-	while( predane == nullptr );
+	while( !complete );
 	return predane;
 }
 
@@ -237,12 +284,15 @@ static void skladani_policek (
 		
 //		my_sleep ( 1 ); // pocka vypocitanou hodnotu
 		thinking_simulation( postup.size() , pozice , pocet_neuspesnych_in_row );
+		std::cout << '-' << std::flush;
 		predane = rozhrani.preber_usporadani(); // zjisti jestli se posunulo
 
 		if ( postup.size() - 1 == pozice && predane == nullptr ) break; // posledni 
 
 		while ( predane == nullptr )  // uprostred animace -> cekej
-		{ predane = rozhrani.preber_usporadani(); my_sleep( 0.001 ); }
+		{
+			std::cout << '-' << std::flush;
+			predane = rozhrani.preber_usporadani(); my_sleep( 0.001 ); }
 
 		if ( memcmp( predane , aktualni.second.get_usporadani() , 4*4 ) == 0 ) 
 		{ 	// nikam se nepohlo znovu klik
@@ -277,10 +327,13 @@ static void skladani_policek (
 // uspe program 
 static void my_sleep ( float time )
 {
+#ifndef NDEBUG
+	std::cout << "sleep : " << time << std::endl;
+#endif
 	unsigned int sec = (int)floor(time);
 	unsigned int usec = (int)floor( (time - floor(time)) * 1e6 );
 
-	assert ( sec < 10 );
+	assert ( sec < 30 );
 	assert ( usec < 1000000 );
 
 	if ( sec )
